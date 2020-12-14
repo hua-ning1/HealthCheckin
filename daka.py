766280ca8920eba1bbaa5a72d76a191eda342033
Tishacy <Tishacy@gmail.com>
2020-12-14 21:21:24 +0800

    Update changes and README.

--- daka.py
+++ daka.py
@@ -60,10 +60,10 @@ class DaKa(object):
         if not html:
             res = self.sess.get(self.base_url)
             html = res.content.decode()
-        
+
         try:
-            old_info = json.loads(re.findall(r'oldInfo: ({[^\n]+})', html)[0])
-            new_info_tmp = json.loads(re.findall(r'def = ({[^\n]+})', html)[0])
+            # old_info = json.loads(re.findall(r'oldInfo: ({[^}]+})', html)[0])
+            new_info_tmp = json.loads(re.findall(r'def = ([^;]+)', html)[0])
             new_id = new_info_tmp['id']
             name = re.findall(r'realname: "([^\"]+)",', html)[0]
             number = re.findall(r"number: '([^\']+)',", html)[0]
@@ -72,7 +72,7 @@ class DaKa(object):
         except json.decoder.JSONDecodeError as err:
             raise DecodeError('JSON decode error')
 
-        new_info = old_info.copy()
+        new_info = new_info_tmp.copy()
         new_info['id'] = new_id
         new_info['name'] = name
         new_info['number'] = number
