d2596eec607ac706d65f677396256a00f7d2c412
Zhouwy <zwysun@outlook.com>
2021-01-07 17:48:10 +0800

    Update daka.py

--- daka.py
+++ daka.py
@@ -193,12 +193,8 @@ def main():
         return 
 
 
-def main_handler(event, context):
-  return main()
-
 if __name__=="__main__":
     try:
-        send_message("begin!")
         main()
     except (KeyboardInterrupt, SystemExit):
         pass
